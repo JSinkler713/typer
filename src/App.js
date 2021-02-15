374154d97f3e42abbe12e647c648516d4614652a
James Sinkler <jamessinkler713@gmail.com>
2021-02-14 16:49:48 -0800

    start to get logic for typing going

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+
+const littleSentence = 'sequelize db:migrate'
+const littleSentenceArray = littleSentence.split('')
 
 function App() {
+  const [keydown, setKeyDown] = useState()
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [typed, setTyped] = useState([])
+  const [notTyped, setNotTyped] = useState(littleSentenceArray)
+  const downHandled = (e) => {
+    setKeyDown(e.key)
+    if(e.key === littleSentence[currentIndex]) {
+      //move to typed
+      setTyped( [...typed, e.key])
+      //update not typed
+      const wasTyped = [...typed]
+      const newTyped = wasTyped.slice(1, wasTyped.length - 1)
+      setNotTyped(newTyped)
+      //increase the index
+      setCurrentIndex(currentIndex + 1)
+
+      //move to the next letter
+    }
+  }
+  useEffect(()=> {
+      window.addEventListener('keydown', downHandled)
+    return ()=> {
+      window.removeEventListener('keydown', downHandled)
+    }
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Current letter just typed</p>
+      <p>{littleSentence}</p> 
+      <p>{typed}</p> 
+      <p>{keydown}</p>
+      <p>Hello</p>
     </div>
   );
 }
