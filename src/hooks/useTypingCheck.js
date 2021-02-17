b16516ad9677d0673b9b667d4474fd4b31a3aa64
James Sinkler <jamessinkler713@gmail.com>
2021-02-17 09:54:12 -0800

    updates to cycle through multiple snippets and move on to the next automatically

--- src/hooks/useTypingCheck.js
+++ src/hooks/useTypingCheck.js
@@ -1,23 +1,26 @@
 import {useState, useEffect, useContext} from 'react'; 
+import snippets from '../data/snippets.js'
+
+console.log(snippets)
  
-const littleSentence = `function() { console.log('hello') }`
-const littleSentenceArray = littleSentence.split('')
 
 const useTypingCheck = ()=> { 
+  const [littleSentence, setLittleSentence] = useState(snippets[0].text)
+  const [snippetIndex, setSnippetIndex] = useState(0)
+
   const [keydown, setKeyDown] = useState()
-  const [currentIndex, setCurrentIndex] = useState(0)
+  const [endingNum, setEndingNum] = useState(snippets[0].text.length)
   const [typed, setTyped] = useState([])
-  const [notTyped, setNotTyped] = useState(littleSentenceArray)
+  const [notTyped, setNotTyped] = useState(littleSentence.split())
   const [numCorrect, setNumCorrect] = useState(0)
 
   const downHandled = (e) => {
     setKeyDown(e.key)
-    if(e.key === littleSentence[currentIndex]) {
+    if(e.key === littleSentence[numCorrect]) {
       setTyped([...typed, e.key])
-      const newTyped = littleSentenceArray.slice(numCorrect+ 1, littleSentenceArray.length)
+      const newTyped = littleSentence.slice(numCorrect+ 1, littleSentence.length)
       setNumCorrect(numCorrect + 1)
       setNotTyped(newTyped)
-      setCurrentIndex(currentIndex + 1)
     }
   }
 
@@ -28,7 +31,30 @@ const useTypingCheck = ()=> {
     window.removeEventListener('keydown', downHandled)
     }
   })
-  return [keydown, currentIndex, typed, notTyped, numCorrect]
+
+  useEffect(()=> {
+    if (numCorrect == endingNum) {
+      // then we got to the end of the current string
+      // reset
+      setNumCorrect(0)
+      if (snippetIndex + 1 < snippets.length) {
+        // update to next snippet if they are there
+        setSnippetIndex(snippetIndex + 1)
+        setLittleSentence(snippets[snippetIndex+1].text)
+      } else {
+        console.log('got through all of them')
+      }
+    }
+  }, [numCorrect])
+
+  useEffect(()=> {
+    // if littleSentence updates call this
+    setTyped([])
+    setNotTyped(littleSentence.split())
+    setEndingNum(snippets[snippetIndex].text.length)
+  }, [littleSentence, setLittleSentence])
+
+  return [keydown, endingNum, typed, notTyped, numCorrect]
 
 } 
 export default useTypingCheck;
