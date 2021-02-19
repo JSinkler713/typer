939755cf6614c9cd34023d941deb8d7def66c014
James Sinkler <jamessinkler713@gmail.com>
2021-02-18 21:55:12 -0800

    trying background colors out

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import useTypingCheck from './hooks/useTypingCheck';
 function App() {
   const [keydown, endingNum, typed, notTyped, numCorrect, doneSnippets] = useTypingCheck()
 
-  const doneSnippetParagraphs = doneSnippets.map(snippet => <p className='space green'>{snippet}</p>)
+  const doneSnippetParagraphs = doneSnippets.map(snippet => <p className='space green typed'>{snippet}</p>)
   return (
     <div className="App">
       <h1 className='nunito'>Welcome to typer</h1>
         { doneSnippets.length ? doneSnippetParagraphs : ''}
-        <p><code className='space green'>{typed}</code><code className='space red'>{notTyped}</code></p> 
+        <p><code className='space green typed'>{typed}</code><code className='space red not-typed'>{notTyped}</code></p> 
     </div>
   );
 }
