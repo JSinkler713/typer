250f2d709166957f030964abceed5960824b8ede
James Sinkler <jamessinkler713@gmail.com>
2021-02-21 09:41:58 -0800

    grabs whole file from cloudinary with fetch in useEffect hook

--- src/hooks/useTypingCheck.js
+++ src/hooks/useTypingCheck.js
@@ -1,16 +1,18 @@
 import {useState, useEffect, useContext} from 'react'; 
 import snippets from '../data/snippets.js'
+import reactFile from '../files/useTypingCheck.js'
 
 console.log(snippets)
- 
+
 
 const useTypingCheck = ()=> { 
-  const [littleSentence, setLittleSentence] = useState(snippets[0].text)
+  const [snippets, setSnippets] = useState([])
+  const [littleSentence, setLittleSentence] = useState('')
   const [snippetIndex, setSnippetIndex] = useState(0)
   const [doneSnippets, setDoneSnippets] = useState([])
 
   const [keydown, setKeyDown] = useState()
-  const [endingNum, setEndingNum] = useState(snippets[0].text.length)
+  const [endingNum, setEndingNum] = useState(100)
   const [typed, setTyped] = useState([])
   const [notTyped, setNotTyped] = useState(littleSentence.split())
   const [numCorrect, setNumCorrect] = useState(0)
@@ -25,6 +27,24 @@ const useTypingCheck = ()=> {
     }
   }
 
+  const turnToText = (file)=> {
+    fetch('https://res.cloudinary.com/dbbthq6ra/raw/upload/v1613927137/useTypingCheck_rrttoi.js')
+      .then(file=> file.text())
+      .then(resultingText=> {
+        console.log(resultingText)
+        const snippetsArray = resultingText.split('\n')
+        console.log(snippetsArray)
+        setSnippets(snippetsArray)
+        setLittleSentence(snippetsArray[0])
+        setEndingNum(snippetsArray[0].length)
+        return snippetsArray
+      })
+      .catch((err)=> {
+        console.log(err)
+      })
+  }
+
+
   useEffect(()=> {
     window.addEventListener('keydown', downHandled)
     // cleanup function on unmount
@@ -32,6 +52,10 @@ const useTypingCheck = ()=> {
     window.removeEventListener('keydown', downHandled)
     }
   })
+  useEffect(()=> {
+    // updates snippets, littleSentence, and endingNum
+    turnToText(reactFile)
+  }, [])
 
   useEffect(()=> {
     if (numCorrect == endingNum) {
@@ -42,21 +66,22 @@ const useTypingCheck = ()=> {
       if (snippetIndex + 1 < snippets.length) {
         // update to next snippet if they are there
         setSnippetIndex(snippetIndex + 1)
-        setLittleSentence(snippets[snippetIndex+1].text)
+        setLittleSentence(snippets[snippetIndex+1])
       } else {
         setTyped([])
         setNotTyped([])
-
         console.log('got through all of them')
       }
     }
-  }, [numCorrect])
+  }, [numCorrect, endingNum])
 
   useEffect(()=> {
     // if littleSentence updates call this
-    setTyped([])
-    setNotTyped(littleSentence.split())
-    setEndingNum(snippets[snippetIndex].text.length)
+    if (snippets.length > 0) {
+      setTyped([])
+      setNotTyped(littleSentence.split())
+      setEndingNum(snippets[snippetIndex].length)
+    }
   }, [littleSentence, setLittleSentence])
 
   return [keydown, endingNum, typed, notTyped, numCorrect, doneSnippets]
