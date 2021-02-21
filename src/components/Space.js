5b5391d73e378d73238c9e99fe683c96d5ff055c
James Sinkler <jamessinkler713@gmail.com>
2021-02-21 15:53:13 -0800

    add whitespace component

---
+++ src/components/Space.js
@@ -0,0 +1,8 @@
+import React, {useState, useEffect, useContext} from 'react'; 
+ 
+const Space = ()=> { 
+  return ( 
+     <span className='green space whitespace'></span> 
+  ) 
+} 
+export default Space;
