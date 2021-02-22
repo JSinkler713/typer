14b3091a95f49b2eea38b876cb2975fb8c41a73e
James Sinkler <jamessinkler713@gmail.com>
2021-02-21 17:10:45 -0800

    add in Space component to indent lines where appropriate

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import './App.css';
 import { useState, useEffect } from 'react'
 import useTypingCheck from './hooks/useTypingCheck';
+import Space from './components/Space';
 
 
 function App() {
-  const [keydown, endingNum, typed, notTyped, numCorrect, doneSnippets] = useTypingCheck()
+  const [keydown, endingNum, typed, notTyped, numCorrect, doneSnippets, spaces] = useTypingCheck()
 
-  const doneSnippetParagraphs = doneSnippets.map(snippet => <p className='space green typed'>{snippet}</p>)
+  const doneSnippetParagraphs = doneSnippets.map(snippet =>{
+    let someSpaces = []
+    for (let i=0; i< snippet.spaces; i++) {
+      someSpaces.push(<Space />)
+    }
+    return(
+    <p className='done'>
+      {someSpaces}
+      <span className='space green typed'>{snippet.littleSentence}</span>
+    </p>
+    )
+  })
+
+  // code block already has a space
+  let leaders
+  if (spaces > 0) {
+    leaders = new Array(spaces - 1)
+    console.log(leaders)
+    leaders.fill(<Space/>)
+    console.log(leaders)
+  }
+
+
   return (
     <div className="App">
       <h1 className='nunito'>Welcome to typer</h1>
         { doneSnippets.length ? doneSnippetParagraphs : ''}
-        <p><code className='space green typed'>{typed}</code><code className='space red not-typed'>{notTyped}</code></p> 
+      <p className='done'><code className='space green typed'>{spaces ? leaders :''}{typed}</code><code className='space red not-typed'>{notTyped}</code></p> 
     </div>
   );
 }
