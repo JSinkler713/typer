0ba444609e0d4b15c182694a561aea4ee68631c9
James Sinkler <jamessinkler713@gmail.com>
2021-03-20 18:21:46 -0700

    edits to check output

--- src/hooks/cleanupWhitespace.js
+++ src/hooks/cleanupWhitespace.js
@@ -2,10 +2,17 @@ import {useState, useEffect, useContext} from 'react';
 
 const cleanupWhitespace = (origLine)=> {
     let count = 0;
+    console.log('***********************')
+    console.log('original line')
+    console.log(origLine)
+    console.log('***********************')
     while( origLine[count] === ' ') {
       count++
     }
   const cleanedLine = origLine.slice(count, origLine.length)
+  if (cleanedLine == undefined) {
+    console.log('hello')
+  }
   console.log(cleanedLine, count)
   return [cleanedLine, count]
 }
