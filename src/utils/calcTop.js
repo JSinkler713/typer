549d87a0c6141cbea7b52d0c0a40e05f89f75c97
James Sinkler <jamessinkler713@gmail.com>
2021-04-09 18:12:23 -0700

    edits to spacing on top

--- src/utils/calcTop.js
+++ src/utils/calcTop.js
@@ -1,7 +1,7 @@
 const calcTop = (numOfSnippetsDone) => {
   // if snippets over 5, start adding 26px negative to top, to scroll up
-  if (numOfSnippetsDone > 5) {
-    let top = (numOfSnippetsDone - 5)*26
+  if (numOfSnippetsDone > 4) {
+    let top = (numOfSnippetsDone - 4)*26
     top = '-' + top + 'px'
     return top
   } else {
